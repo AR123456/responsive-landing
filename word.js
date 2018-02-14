dff1618fee222e45f5151c7946f5cea09c2a1097
AR123456 <coloradoz99@gmail.com>
2018-02-14 15:47:08 -0700

    logic update

--- word.js
+++ word.js
@@ -1,8 +1,7 @@
 
 //required packages 
 var inquirer = require('inquirer');
-// require letter objects
-var Letter = require('./letter.js');
+
 //  * **Word**: Used to create an object representing the current word the user is attempting to guess. This should contain word specific logic and data.
 function Word() {
     console.log( "You are linked to the word.js file  ");
