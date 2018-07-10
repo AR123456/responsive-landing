34874eae0b98186b33463476a00c6b772e2fdcba
AR123456 <coloradoz99@gmail.com>
2018-07-09 19:14:30 -0600

    updates to  app. js

--- app.js
+++ app.js
@@ -1,31 +1,20 @@
-// Load the inquirer pacakge
 var inquirer = require("inquirer");
 var fs = require("fs");
-// Link in the letters to display
 var checkForLetter = require("./letter.js");
-//require wor.js file
 var lettersDisplay = require("./word.js");
-//link to array of words
 var wordArray = require("./array.js");
-// Link in the letters to display
 var checkForLetter = require("./letter.js");
 var badGuess = [];
 var matchingLetters = [];
 var underscoreWord;
-
-//start game get word from the array
 var game = {
-  wordArray: wordArray, //  list of words
-  guessesLeft: 9, // per word
-  currentWord: null, // the word object
+  wordArray: wordArray,
+  guessesLeft: 9,
+  currentWord: null,
   startGame: function() {
-    // console.log("you are in the startGame function ");
-    //set guesses to 9
     this.guessesLeft = 9;
-    // get a  word from array
     var i = Math.floor(Math.random() * this.wordArray.length);
     this.currentWord = this.wordArray[i];
-    //show word for testing
     console.log("  ");
     console.log(
       "\x1b[34m%s\x1b[0m",
@@ -33,24 +22,19 @@ var game = {
     );
     console.log("________________________________________");
     console.log("  ");
-    //Display start game
     console.log(
       "\x1b[33m%s\x1b[0m",
       "Its time to play neuro anatomy Hangman, guess a letter"
     );
-    // make the underscore word object.
     underscoreWord = new lettersDisplay(this.currentWord);
-    //send underscoreWord object to setDispaly functon
     underscoreWord.setDisplay();
     console.log(
       "\x1b[36m%s\x1b[0m",
       "You have " + game.guessesLeft + " guesses left"
     );
-    // prompt for a letter
     promptUser();
   }
 };
-///start of game prompt
 inquirer
   .prompt([
     {
@@ -62,7 +46,6 @@ inquirer
   ])
   .then(function(answers) {
     var action = answers.wanaPlay;
-    // console.log(action);
     switch (action) {
       case "yes":
         game.startGame();
@@ -72,12 +55,8 @@ inquirer
         break;
     }
   });
-///prompt to get letters input
-//prompt user function
 function promptUser() {
-  //put  gap between inputs
   console.log(" ");
-  //  prompt for new letter
   if (game.guessesLeft > 0) {
     inquirer
       .prompt([
@@ -88,10 +67,8 @@ function promptUser() {
         }
       ])
       .then(function(userInput) {
-        // get guess
         var letterGuessed = userInput.letter.toLowerCase();
         if (badGuess.indexOf(letterGuessed) != -1) {
-          // if a repeat letter
           console.log(" ");
           console.log(
             "\x1b[31m%s\x1b[0m",
@@ -105,12 +82,9 @@ function promptUser() {
           promptUser();
         } else {
           badGuess.push(letterGuessed);
-          // Check matching letter in the word
           var letterInWord = checkForLetter(letterGuessed, game.currentWord);
-          // If the letter is in the word, update display
           if (letterInWord) {
             matchingLetters.push(letterGuessed);
-            // display
             console.log(" ");
             console.log("\x1b[32m%s\x1b[0m", "Correct!!!");
             console.log(" ");
@@ -119,7 +93,6 @@ function promptUser() {
               matchingLetters
             );
             underscoreWord.setDisplay();
-            // Test if the user has won
             if (underscoreWord.winner) {
               console.log(" ");
               console.log(
@@ -127,11 +100,7 @@ function promptUser() {
                 "You won " + game.currentWord + " is the word!"
               );
               console.log(" ");
-              //  game.startGame();// no way to exit with this
-              // return;
-            }
-            // check for duplicate guess and propmtUser
-            else {
+            } else {
               console.log(" ");
               console.log(
                 "\x1b[36m%s\x1b[0m",
@@ -143,12 +112,9 @@ function promptUser() {
                 "Letters already guessed: " + badGuess
               );
               console.log(" ");
-
               promptUser();
             }
-          }
-          // promps User and decrement guesses
-          else {
+          } else {
             console.log(" ");
             console.log("\x1b[31m%s\x1b[0m", "That letter is not in the word");
             console.log(" ");
@@ -169,9 +135,7 @@ function promptUser() {
           }
         }
       });
-  }
-  // game over loss
-  else {
+  } else {
     console.log(" ");
     console.log(
       "\x1b[31m%s\x1b[0m",
