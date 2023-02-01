1a1ef22a2f3a40878f106912309913e63376b2fd
AR123456 <coloradoz99@gmail.com>
2023-02-01 06:21:09 -0700

    clearing out old repo

--- array.js
+++
@@ -1,5 +0,0 @@
-
-
-var wordArray = ["amygdala","cortex","sagittal","chiasm","gyrus","nerve","brain","horn","cell","occipital","axon","atlas","ataxia","dendrite","diplopia","babinski","medulla","vertigo","uncus","glia","dendrite","proprioception","neuron"];
-
-module.exports = wordArray;
